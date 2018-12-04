6a39d571b75be150f4e47d867f44bd4da253c074
mu102449 <gordon0105@gmail.com>
2018-12-04 15:59:18 +0800

    Remove GO code

--- code_for_TCH_EM.R
+++ code_for_TCH_EM.R
@@ -439,31 +439,3 @@ axis(2)
 abline(h = summary(dat.global$logP)[5],col="red")
 abline(h = summary(dat.local$logP)[5],col="blue")
 dev.off()
-
-#enrichment test-global (top20)
-GO_result_global=c()
-for (i in 1:length(goterm)) {
-  m=length(unique(sgd[sgd$V5==goterm[i],3]))
-  k=length(cor.global[order(cor.global[,3])[1:20],1])
-  x=length(intersect(k,m))
-  n=length(union(cor.global[,1],cor.local[,1]))-m
-  p.val = phyper(x,m,n,k,lower.tail = F)
-  pre=c(goterm[i],p.val)
-  GO_result_global=rbind(GO_result_global,pre)
-}
-colnames(GO_result_global)=c("GO_term","p.value")
-write.table(GO_result_global,file = "./GO_result_global.txt",quote = F,row.names = F)
-
-#enrichment test-local (top20)
-GO_result_local=c()
-for (i in 1:length(goterm)) {
-  m=length(unique(sgd[sgd$V5==goterm[i],3]))
-  k=length(cor.local[order(cor.local[,3])[1:20],1])
-  x=length(intersect(k,m))
-  n=length(union(cor.global[,1],cor.local[,1]))-m
-  p.val = phyper(x,m,n,k,lower.tail = F)
-  pre=c(goterm[i],p.val)
-  GO_result_local=rbind(GO_result_local,pre)
-}
-colnames(GO_result_local)=c("GO_term","p.value")
-write.table(GO_result_local,file = "./GO_result_local.txt",quote = F,row.names = F)
