5dbe6bcbdb2078bb762f85be9020b25954c8e022
mu102449 <gordon0105@gmail.com>
2018-12-05 19:11:22 +0800

    fix plotting margin

--- del_pcc_heatmap.R
+++ del_pcc_heatmap.R
@@ -15,7 +15,7 @@ cor_mat <- 1 - cor(as.matrix(del_arr))
 linkage <- hclust(as.dist(cor_mat), method = "average")
 
 # Plot dendrogram with pcc and average method clustering
-png(filename = "del_arr_dendrogram.png", res = 300)
+png(filename = "./output/del_arr_dendrogram.png", width = 30, height = 15, units = "cm", res = 300)
 plot(hclust(as.dist(cor_mat), method = "average"), hang = -1, labels = FALSE)
 # abline(h = 0.6, col="red")
 dev.off()
@@ -25,10 +25,10 @@ library(pheatmap)
 breaksList = seq(0, 2, by = 0.2)
 col <- colorRampPalette(c("brown", "red", "yellow", "white"))(length(breaksList))
 
-png(filename = "del_arr_heatmap.png", res = 300)
+png(filename = "./output/del_arr_heatmap.png", width = 25, height = 25, units = "cm", res = 300)
 pheatmap(mat = cor_mat[linkage$order, linkage$order], col = col,
          cluster_rows = FALSE, cluster_cols = FALSE,
          show_rownames = FALSE, show_colnames = FALSE,
-         cellwidth = 0.8, cellheight = 0.8,
+         cellwidth = 0.9, cellheight = 0.9,
          breaks = breaksList)
 dev.off()
